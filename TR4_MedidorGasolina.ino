bbb48fe751f2df4c2946a8da379b6b4b962d790a
Mariana Guimaraes <32623226+marianaguimaraes@users.noreply.github.com>
2020-09-28 12:16:51 -0300

    Add files via upload

---
+++ TR4_MedidorGasolina.ino
@@ -0,0 +1,64 @@
+// the value of the 'other' resistor
+#define SERIESRESISTOR 1000    
+ 
+// What pin to connect the sensor to
+char dataString[50] = {0};
+int ohm =0 ;
+int pwmpin = 3;
+int auxconvert =0;
+int convert =0;
+int convert2 =0;
+float vout = 0;
+int i = 0;
+void setup(){
+  
+Serial.begin(9600);
+pinMode(pwmpin,OUTPUT);  
+
+}
+
+void loop(){
+
+
+float reading;
+float media = 0; 
+reading = analogRead(A0);
+
+reading = (1023 / reading)  - 1;     // (1023/ADC - 1) 
+reading = 1000 / reading;  // 10K / (1023/ADC - 1)
+
+for (i = 0; i < 5; i++) {
+  reading = analogRead(A0);
+  reading = (1023 / reading)  - 1;     // (1023/ADC - 1) 
+  reading = 1000 / reading;  // 10K / (1023/ADC - 1)
+  
+  media = media + reading;
+  //Serial.println(reading);
+  delay(3000);
+}
+float R2 = media/5; 
+int convert =0;
+if(i > 4){
+    float vout = (convert*5.0)/255.0;
+    if(R2 > 0)
+    {      
+      convert =2.9474*R2-20.034 ;
+      vout = ((convert*5.0)/255.0)+2.0;
+
+      if (convert < 0) {convert = 0;}
+      if (convert > 255) {convert = 255;}
+      analogWrite(pwmpin,convert); 
+      //Serial.print("Vout: ");
+      //Serial.print(vout);
+      //Serial.print("  - R2: ");
+      //Serial.print(R2);
+      //Serial.print("  - Convert: ");
+      //Serial.println(convert);
+      ohm = (int)R2;
+      sprintf(dataString,"%02X",ohm); // convert a value to hexa 
+      Serial.println(dataString);   // send the data
+     }      
+  }
+  //analogWrite(pwmpin2,255);
+  i++;
+}
